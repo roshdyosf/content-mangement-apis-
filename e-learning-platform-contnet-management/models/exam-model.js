001a792e3cfb574e1d1f1163f349e52ed49e1433
roshdyosf <yosef2236303@gmail.com>
2025-03-20 09:30:54 +0200

    creating model for exams

---
+++ e-learning-platform-contnet-management/models/exam-model.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const mcqSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    choices: [{
+        type: String,
+        required: true,
+        trim: true,
+    }],
+    answer: {
+        type: String,
+        required: true,
+        trim: true,
+    }
+});
+
+const examSchema = new mongoose.Schema({
+    educatorId: {
+        type: String,
+        required: true,
+    },
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+        required: true,
+    },
+    sectionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Section',
+        required: true,
+    },
+    videoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video',
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    mcq: [mcqSchema],
+}, { timestamps: true });
+const Exam = mongoose.model('Exam', examSchema)
+module.exports = Exam

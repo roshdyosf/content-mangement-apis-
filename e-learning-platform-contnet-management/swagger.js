beee4ffae032aa9650fbea9555f3d25731acdde8
roshdyosf <yosef2236303@gmail.com>
2025-06-16 13:26:54 +0300

    feat: add swagger.js for automatic API documentation generation

---
+++ e-learning-platform-contnet-management/swagger.js
@@ -0,0 +1,21 @@
+// swagger.js
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'E-Learning Platform Content Management API',
+        description: 'API documentation for the E-Learning Platform Content Management system'
+    },
+    host: 'localhost:5008',
+    schemes: ['http'],
+};
+
+const outputFile = './swagger-output.json';
+const endpointFiles = ['./server.js'];
+
+swaggerAutogen(outputFile, endpointFiles, doc).then(() => {
+    require('./server.js');
+    console.log('Swagger documentation generated!');
+
+})
+

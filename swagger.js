f76ce299b189a62e843dded45bc2b3488d4708c9
Blackdukee <1213059@dtu.edu.eg>
2025-06-19 18:10:00 +0300

    swagger

--- swagger.js
+++ swagger.js
@@ -1,21 +1,25 @@
 // swagger.js
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
-    info: {
-        title: 'E-Learning Platform Content Management API',
-        description: 'API documentation for the E-Learning Platform Content Management system'
+  info: {
+    title: "E-Learning Platform Content Management API",
+    description:
+      "API documentation for the E-Learning Platform Content Management system",
+  },
+  host: "http://18.184.52.10:5008",
+  schemes: ["http"],
+  security: [
+    {
+      BearerAuth: [],
     },
-    host: '3.70.227.2:5008',
-    schemes: ['http'],
+  ],
 };
 
-const outputFile = './swagger-output.json';
-const endpointFiles = ['./server.js'];
+const outputFile = "./swagger-output.json";
+const endpointFiles = ["./server.js"];
 
 swaggerAutogen(outputFile, endpointFiles, doc).then(() => {
-    require('./server.js');
-    console.log('Swagger documentation generated!');
-
-})
-
+  require("./server.js");
+  console.log("Swagger documentation generated!");
+});
